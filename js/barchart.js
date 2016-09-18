57aaac595087783301c587373e56fc3ce93636dd
JosephLivengood <JoeyLivengood@gmail.com>
2016-09-18 00:44:44 -0400

    Formatted to fit on screen
    Handles script unsecure error.

--- js/barchart.js
+++ js/barchart.js
@@ -1,13 +1,9 @@
 //SUPER SUPER SUPER WIP
-var data = [{label:"1947-01-01", value:243.1},
-        {label:"1947-04-01", value:246.3},
-        {label:"1947-07-01", value:250.1},
-        {label:"1947-10-01", value:260.3}];
-var datapoints = [];
-/* function Datasetpoint(label, value) {
-    this.label = label;
-    this.value = value;
-}*/
+var data = [];
+
+/*
+* getData() is NOT async. We need to load the full data set before trying to draw.
+*/
 function getData() {
     $.ajax(
     {
@@ -27,27 +23,25 @@ function getData() {
     }
     )
 };
-
-getData();
-
-console.log('strt');
+    
+    d3.select("#script-warning").html(" ");
+    getData();
     var div = d3.select("body").append("div").attr("class", "toolTip");
-    var axisMargin = 20,
-            margin = 4,
-            valueMargin = 4,
+    var axisMargin = 10,
+            margin = 10,
+            valueMargin = 10,
             width = parseInt(d3.select('body').style('width'), 10),
             height = parseInt(d3.select('body').style('height'), 10),
-            barHeight = (height-axisMargin-margin*2)* 0.4/data.length,
-            barPadding = (height-axisMargin-margin*2)*0.6/data.length,
+            barHeight = (height-axisMargin-margin*2)* 0.9/data.length,
+            barPadding = (height-axisMargin-margin*2)*0.0/data.length,
             data, bar, svg, scale, xAxis, labelWidth = 0;
 
     max = d3.max(data, function(d) { return d.value; });
 
-    svg = d3.select('body')
+    svg = d3.select('.barchart')
             .append("svg")
             .attr("width", width)
             .attr("height", height);
-
 
     bar = svg.selectAll("g")
             .data(data)
@@ -65,13 +59,17 @@ console.log('strt');
             .attr("y", barHeight / 2)
             .attr("dy", ".35em") //vertical align middle
             .text(function(d){
-                return d.label;
+                if (d.label.includes('-04-')){ //we only want 1 label per year to keep it not crowded
+                    return d.label.substring(0,4)+".";
+                } else {
+                    return;
+                }   
             }).each(function() {
         labelWidth = Math.ceil(Math.max(labelWidth, this.getBBox().width));
     });
 
     scale = d3.scale.linear()
-            .domain([0, max])
+            .domain([0, 19000])
             .range([0, width - margin*2 - labelWidth]);
 
     xAxis = d3.svg.axis()
@@ -89,11 +87,14 @@ console.log('strt');
     bar.append("text")
             .attr("class", "value")
             .attr("y", barHeight / 2)
-            .attr("dx", -valueMargin + labelWidth) //margin right
+            .attr("dx", -valueMargin + labelWidth - 50) //margin right
             .attr("dy", ".35em") //vertical align middle
             .attr("text-anchor", "end")
             .text(function(d){
-                return (d.value+"m");
+                if(d.label == "2010-07-01") {
+                    return "Bad banking policy and massive bank bailouts!";
+                }
+                return; //(d.value+"m"); //@/1000
             })
             .attr("x", function(d){
                 var width = this.getBBox().width;
@@ -105,8 +106,9 @@ console.log('strt');
                 div.style("left", d3.event.pageX+10+"px");
                 div.style("top", d3.event.pageY-25+"px");
                 div.style("display", "inline-block");
-                div.html((d.label)+"<br>"+(d.value)+"%");
+                div.html((d.label)+"<br>"+(d.value)+"m");
             });
+
     bar
             .on("mouseout", function(d){
                 div.style("display", "none");
@@ -114,5 +116,5 @@ console.log('strt');
 
     svg.insert("g",":first-child")
             .attr("class", "axisHorizontal")
-            .attr("transform", "translate(" + (margin + labelWidth) + ","+ (height - axisMargin - margin)+")")
+            .attr("transform", "translate(" + (margin + labelWidth) + ","+ (height - axisMargin - margin - 100)+")")
             .call(xAxis);
