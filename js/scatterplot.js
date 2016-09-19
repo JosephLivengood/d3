3852decff22a92a7e0e6a74b6178680e6f8f4f6e
JosephLivengood <JoeyLivengood@gmail.com>
2016-09-19 00:57:35 -0400

    Tooltips
    Minor formatting and addition of tooltips!

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -14,7 +14,21 @@ var yAxis = d3.svg.axis().scale(y)
 var valueline = d3.svg.line()
     .x(function(d) { return x(d.Seconds); })
     .y(function(d) { return y(d.Place); });
-    
+
+var tip = d3.select('body')
+    .append('div')
+    .attr('class', 'tip')
+    .style('border', '1px solid steelblue')
+    .style('padding', '5px')
+    .style('position', 'absolute')
+    .style('display', 'none')
+    .on('mouseover', function(d, i) {
+            tip.transition().duration(0);
+      })
+      .on('mouseout', function(d, i) {
+            tip.style('display', 'none');
+      });
+
 var svg = d3.select("body")
     .append("svg")
         .attr("width", width + margin.left + margin.right)
@@ -30,11 +44,13 @@ d3.csv("./resources/scatter.csv", function(error, data) {
         console.log(d);
         d.Seconds = +d.Seconds - 2210; //this gives seconds past leading time(hardcoded)
         d.Place = +d.Place;//+d.close;
-        d.Doping = d.Doping;
+        if (d.Doping == "") {
+            d.Doping = "CLEAN!";
+        }
     });
 
     //x.domain(d3.extent(data, function(d) { return d.Seconds; }));
-    x.domain([0, 200]); //This data wont ever be changed, lets just hard code the domain.
+    x.domain([0, 210]); //This data wont ever be changed, lets just hard code the domain.
     y.domain([0, d3.max(data, function(d) { return d.Place; })]);
 
     /* A path line doesn't make too much sense looking at it
@@ -46,23 +62,35 @@ d3.csv("./resources/scatter.csv", function(error, data) {
     svg.selectAll("dot")
         .data(data)
       .enter().append("circle")
-        .attr("r", 4.5)
+        .attr("r", 6.5)
         .attr("cx", function(d) { return x(d.Seconds); })
         .attr("cy", function(d) { return y(d.Place); })
+        .on('mouseover', function(d) {
+            tip.transition().duration(0)
+            tip.style('top', '100px') //y(d.Place) - 20 + 'px')
+            tip.style('left', '100px') //x(d.Seconds) + 'px')
+            tip.style('display', 'block')
+            tip.html('<b>'+d.Name+'</b><br>'+d.Year+'<br>'+d.Time+'<br><i>'+d.Doping+'</i>')
+        })
+        .on('mouseout', function(d) {
+            tip.transition()
+            .delay(1000)
+            .style('display', 'none');
+        })
         .style("fill", function(d) {
-            if (d.Doping == "") {
+            if (d.Doping == "CLEAN!") {
                 return "green";
             }
                 return "red";
-        })
+        });
 
     svg.selectAll("text")
         .data(data)
       .enter().append("text")
-        .text(function(d) { return d.Name; })
+        .text(function(d) { return d.Name+", "+d.Nationality; })
         .attr("x", function(d) { return x(d.Seconds); })
         .attr("y", function(d) { return y(d.Place); })
-        .attr("transform", "translate(5,+4)")
+        .attr("transform", "translate(8,+3)")
         .style("font-weight", function(d) { return (d.Doping == "") ? "bold" : ""; });
     
     svg.append("g")
@@ -73,5 +101,4 @@ d3.csv("./resources/scatter.csv", function(error, data) {
     svg.append("g")
         .attr("class", "y axis")
         .call(yAxis);
-
 });
